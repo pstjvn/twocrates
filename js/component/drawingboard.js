b783f85e4cda5b0c166d79eeec407fe05923e194
Peter StJ <regardingscot@gmail.com>
2013-04-05 17:26:35 +0300

    Adds project specific frame (with offsets) and project specific drawing sheet.

---
+++ js/component/drawingboard.js
@@ -0,0 +1,24 @@
+goog.provide('k3d.component.DrawingBoard');
+
+goog.require('pstj.ui.TouchSheet');
+
+/**
+ * The drawing board for the kitchen.
+ * @constructor
+ * @extends {pstj.ui.TouchSheet}
+ */
+k3d.component.DrawingBoard = function() {
+  goog.base(this);
+};
+goog.inherits(k3d.component.DrawingBoard, pstj.ui.TouchSheet);
+
+goog.scope(function() {
+
+  var _ = k3d.component.DrawingBoard.prototype;
+
+  /** @inheritDoc */
+  _.addChild = function(child, render) {
+    goog.asserts.assertInstanceof(child, k3d.component.Item);
+    goog.base(this, 'addChild', child, render);
+  };
+});
