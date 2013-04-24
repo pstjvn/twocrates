bcb646d08820f71c940f5a4395a992f08173747d
Peter StJ <regardingscot@gmail.com>
2013-04-24 22:40:18 +0300

    Add cloning method

--- js/ds/item.js
+++ js/ds/item.js
@@ -1,6 +1,7 @@
 goog.provide('k3d.ds.Item');
 
 goog.require('pstj.ds.ListItem');
+goog.require('goog.object')
 
 /**
  * The item that we have on the drawing board.
@@ -12,6 +13,14 @@ k3d.ds.Item = function(data) {
   goog.base(this, data);
 };
 goog.inherits(k3d.ds.Item, pstj.ds.ListItem);
+
+/**
+ * Clones the record type's raw date.
+ * @return {Object} The raw record data cloned.
+ */
+k3d.ds.Item.prototype.clone = function() {
+  return goog.asserts.assertObject(goog.object.unsafeClone(this.getRawData()));
+};
 
 /**
  * The properties of the item.
