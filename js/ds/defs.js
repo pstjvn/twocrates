f29ed915f0e6901b01716d62e94b2404c4f87075
Peter StJ <regardingscot@gmail.com>
2013-04-24 22:40:03 +0300

    Add definitions for errors and data structure symbol names

--- js/ds/defs.js
+++ js/ds/defs.js
@@ -11,3 +11,40 @@ goog.provide('k3d.ds.definitions');
  * @type {number}
  */
 k3d.ds.definitions.headerHeight = 45;
+
+/**
+ * Provides symbol names for the URL paths used in the app.
+ * @enum {string}
+ */
+k3d.ds.definitions.Path = {
+  LOAD_KITCHEN: 'js/testdata.js'
+};
+
+/**
+ * Provides the structure names as symbols.
+ * @enum {string}
+ */
+k3d.ds.definitions.Struct = {
+  KITCHEN: 'kitchen',
+  ID: 'id',
+  WALLS: 'walls',
+  WIDTH: 'width',
+  HEIGHT: 'height',
+  ATTACHED: 'is_attached_to_wall',
+  ITEMS: 'items',
+  TOP_ROW: 'top',
+  BOTTOM_ROW: 'bottom',
+  KITCHEN_PROJECT_ID: 'kitchen_project_id',
+  STATUS: 'status'
+};
+
+/**
+ * THe static string used for errors.
+ * TODO: this should actually be implemented as templates instead
+ * @enum {number}
+ */
+k3d.ds.definitions.Static = {
+  SERVER_HTTP_ERROR: 0,
+  UNPARSABLE_JSON: 1,
+  STRUCTURED_ERROR: 2
+};
