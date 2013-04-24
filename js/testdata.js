1a09249a6ea39ebd7da5924ccf7df52ee5ac8c83
Peter StJ <regardingscot@gmail.com>
2013-04-24 22:37:50 +0300

    Add test data

---
+++ js/testdata.js
@@ -0,0 +1,95 @@
+{
+  "status": 0,
+  "kitchen": {
+    "id": "1",
+    "type": "L",
+    "description": "My test kitchen project",
+    "pictures": [],
+    "number_of_walls": 2,
+    "handles_id": "1",
+    "finish_id": "1",
+    "walls": [{
+      "id": "1",
+      "width": 4000,
+      "height": 2500,
+      "id_attached_to_wall": true,
+      "items": {
+        "top": [{
+          "id": "4",
+          "is_attached_to_wall": true,
+          "description": "Upper row glass cabinet with two shelfs.",
+          "categoty_id": 1,
+          "width": 700,
+          "width2": 400,
+          "height": 440,
+          "depts": 350,
+          "front_picture": "assets/4.png",
+          "angle_picture": "assets/HD_03.jpg",
+          "has_top_board": true,
+          "model_id": "18",
+          "is_spacer": false,
+          "price": 3500,
+          "required_handles": 0
+        }],
+        "bottom": [{
+          "id": "1",
+          "is_attached_to_wall": true,
+          "description": "Floor cabinet, 2 rows, one door, finish could be polished back and cherry, high orders and promotion.",
+          "categoty_id": 2,
+          "width": 300,
+          "width2": 400,
+          "height": 550,
+          "depts": 350,
+          "front_picture": "assets/1.png",
+          "angle_picture": "assets/HD_03.jpg",
+          "has_top_board": true,
+          "model_id": "23",
+          "is_spacer": false,
+          "price": 3500,
+          "required_handles": 0
+        }, {
+          "id": "2",
+          "is_attached_to_wall": true,
+          "description": "Floor cabiner, 2 rows, one door",
+          "categoty_id": 2,
+          "width": 550,
+          "width2": 400,
+          "height": 550,
+          "depts": 350,
+          "front_picture": "assets/2.png",
+          "angle_picture": "assets/HD_03.jpg",
+          "has_top_board": true,
+          "model_id": "23",
+          "is_spacer": false,
+          "price": 3500,
+          "required_handles": 0
+        }, {
+          "id": "3",
+          "is_attached_to_wall": true,
+          "description": "Refrigerator, single door, 300 liters.",
+          "categoty_id": 2,
+          "width": 650,
+          "width2": 400,
+          "height": 1200,
+          "depts": 350,
+          "front_picture": "assets/3.png",
+          "angle_picture": "assets/HD_03.jpg",
+          "has_top_board": true,
+          "model_id": "23",
+          "is_spacer": false,
+          "price": 3500,
+          "required_handles": 0
+        }]
+      }
+    }, {
+      "id": "2",
+      "width": 2500,
+      "height": 2500,
+      "is_attached_to_wall": false,
+      "items": {
+        "top": [],
+        "bottom": []
+      }
+    }]
+  }
+}
