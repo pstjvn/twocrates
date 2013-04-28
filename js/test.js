d4b482ab0569f4cfa19b37dbd4e066865e7151df
Peter StJ <regardingscot@gmail.com>
2013-04-28 16:17:53 +0300

    Remove external references and let the main controler be responsible for the app initialization.

--- js/test.js
+++ js/test.js
@@ -1,46 +1,14 @@
 goog.provide('test');
 
-goog.require('goog.dom');
 goog.require('k3d.control.Main');
-goog.require('k3d.ds.definitions');
-goog.require('k3d.template');
 
 /**
  * @fileoverview Provides the testing bed for our project.
  */
 test = function() {
-  var body = k3d.template.base({});
-  var el = goog.dom.htmlToDocumentFragment(body);
-  document.body.appendChild(el);
-
-  // find header and apply size
-  goog.dom.getElementByClass(goog.getCssName('header')).style.height =
-    k3d.ds.definitions.headerHeight + 'px';
 
   var app = new k3d.control.Main();
 
-  // // Init the error handler. Basically we want to have general app control that
-  // // manages all those controls.
-  // k3d.control.ErrorHandler.getInstance();
-
-  // // load data in editor.
-  // // k3d.control.Editor.getInstance().setWallSize(4000, 2450);
-  // k3d.control.Editor.getInstance().install(document.body);
-
-  // // instantiate the buttons for controls
-  // k3d.control.Buttons.getInstance();
-
-  // k3d.control.Loader.getInstance().getKitchen().addCallback(function(kitchen) {
-
-  //   goog.asserts.assertInstanceof(kitchen, k3d.ds.KitchenProject,
-  //     'Should have been a kitchen');
-
-  //   k3d.control.Editor.getInstance().loadData(kitchen);
-  //   // kitchen.setDescription('New descritpion for project 3');
-  //   // setTimeout(function() {
-  //   //   k3d.control.Loader.getInstance().saveKitchen(kitchen);
-  //   // }, 1000);
-  // });
 
 
   // var k3ditem1 = new k3d.component.Item();
