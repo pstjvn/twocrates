30e76a219fa82a9b68a9332d3c8332b4f77acd76
Peter StJ <regardingscot@gmail.com>
2013-05-03 13:41:33 +0300

    Use custom class names in control group buttons to make them easily styleable.

--- js/component/contolgroup.js
+++ js/component/contolgroup.js
@@ -1,8 +1,22 @@
 goog.provide('k3d.component.ControlButtons');
 
+goog.require('goog.ui.ControlRenderer');
 goog.require('k3d.template');
 goog.require('pstj.ui.Button');
+goog.require('pstj.ui.CustomButtonRenderer');
 goog.require('pstj.ui.Templated');
+
+/**
+ * @fileoverview Provides a control widget with touch enabled buttons inside
+ *   of it. Each button has an 'action' designated to it by using a dataset
+ *   string in the DOM. The buttons are activated by PRESS (triggered by mouse
+ *   or touch events) and the action is then propagated to the enclosing
+ *   component. Controls should listen to the control component for the
+ *   actions. This way we achieve to hide the implementation details of the
+ *   action triggering by from the control instance.
+ *
+ * @author regardingscot@gmail.com (Peter StJ)
+ */
 
 /**
  * The control UI component.
@@ -14,6 +28,15 @@ k3d.component.ControlButtons = function() {
 };
 goog.inherits(k3d.component.ControlButtons, pstj.ui.Templated);
 
+/**
+ * @private
+ * @type {pstj.ui.CustomButtonRenderer}
+ */
+k3d.component.ControlButtons.
+  renderer_ = /** @type {pstj.ui.CustomButtonRenderer} */(
+    goog.ui.ControlRenderer.getCustomRenderer(
+      pstj.ui.CustomButtonRenderer, goog.getCssName(
+        'design-tool-control-button')));
 
 goog.scope(function() {
   var _ = k3d.component.ControlButtons.prototype;
@@ -39,8 +62,9 @@ goog.scope(function() {
    */
   _.createActionButton_ = function(element) {
     element = /** @type {Element} */ (element);
-    var button = new pstj.ui.Button();
+    var button = new pstj.ui.Button(k3d.component.ControlButtons.renderer_);
     this.addChild(button);
     button.decorate(element);
   };
-})
+
+});
