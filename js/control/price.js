56720543b5a06ceb8a33c2232c16ee023513330d
Peter StJ <regardingscot@gmail.com>
2013-05-15 17:49:54 +0300

    Make the price calculation work and implement simplest calculation

--- js/control/price.js
+++ js/control/price.js
@@ -1,6 +1,8 @@
 goog.provide('k3d.control.Price');
 
+goog.require('goog.asserts');
 goog.require('k3d.component.PriceLabelTemplate');
+goog.require('k3d.ds.definitions');
 goog.require('pstj.control.Base');
 goog.require('pstj.ng.Template');
 
@@ -12,10 +14,13 @@ goog.require('pstj.ng.Template');
 k3d.control.Price = function() {
   goog.base(this);
   /**
-   * @private
+   * @protected
    * @type {k3d.ds.KitchenProject}
    */
-  this.data_ = null;
+  this.data = null;
+  this.delay_ = new goog.async.Delay(function() {
+    this.setPrice(this.calculatePrice_());
+  }, 50, this);
   this.label_ = new pstj.ng.Template(
     k3d.component.PriceLabelTemplate.getInstance());
   this.label_.render(document.body);
@@ -26,23 +31,59 @@ goog.addSingletonGetter(k3d.control.Price);
 goog.scope(function() {
 
   var _ = k3d.control.Price.prototype;
+  var Struct = k3d.ds.definitions.Struct;
+
   /**
    * Loads the data of the kitchen.
    * @param {k3d.ds.KitchenProject} kitchen The kitchen project data record.
    */
   _.loadData = function(kitchen) {
-    this.data_ = kitchen;
+    this.data = kitchen;
     this.update();
   };
 
   /**
-   * Recalculaes the price.
+   * Calculates the price of the whole kitchen based on its current structure.
+   * @private
+   * @return {number}
    */
-  _.update = function() {
+  _.calculatePrice_ = function() {
+    // first and foremost collect all item's price.
+    var price = 0;
+    var wallindex = 0;
+    var wall = null;
+    var row = null;
+    while (this.data.hasWallWithIndex(wallindex)) {
+      wall = this.data.getWall(wallindex);
+      row = wall.getRow(true);
+      row.forEach(function(item) {
+        price = price + goog.asserts.assertNumber(item.getProp(Struct.PRICE));
+      });
+      row = wall.getRow(false);
+      row.forEach(function(item) {
+        price = price + goog.asserts.assertNumber(item.getProp(Struct.PRICE));
+      });
+      wallindex++;
+    }
+    return price;
+  };
+
+  /**
+   * Sets the price on the price label.
+   * @param {number} price The new price to display.
+   */
+  _.setPrice = function(price) {
     this.label_.setModel({
       'id': 1,
-      'price': 124399
+      'price': price
     });
+  };
+
+  /**
+   * Trigger price recalculation.
+   */
+  _.update = function() {
+    this.delay_.start();
     // recalculate the price and vizualize! for ech wall iterate bottom row and
     // calculate bench size and kickboard size. sum(bench), sum(kickboard),
     // sum(handles) iterate top row sum(handles) for items that are first - skip
