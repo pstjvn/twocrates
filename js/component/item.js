5e4884559e269f8b52e78ff9abbe97b3f5fe11b8
Peter StJ <regardingscot@gmail.com>
2013-12-19 17:15:45 +0200

    Linter fixes, fix missing dependency, remove unused private property

--- js/component/item.js
+++ js/component/item.js
@@ -7,10 +7,12 @@ goog.require('goog.pubsub.PubSub');
 goog.require('goog.style');
 goog.require('k3d.ds.definitions');
 goog.require('k3d.ds.helpers');
+goog.require('k3d.template');
 goog.require('pstj.ds.ListItem');
 goog.require('pstj.lab.style.css');
 goog.require('pstj.ui.MoveTouch');
 goog.require('pstj.ui.Template');
+
 
 /**
  * @fileoverview Provides the specialized component to represent an item
@@ -19,11 +21,13 @@ goog.require('pstj.ui.Template');
  * @author regardingscot@gmail.com (Peter StJ)
  */
 
+
 /**
  * The pubsub channel for the move of an item.
  * @type {goog.pubsub.PubSub}
  */
 k3d.component.PubSub = new goog.pubsub.PubSub();
+
 
 /**
  * The topic we are publishing on.
@@ -31,6 +35,8 @@ k3d.component.PubSub = new goog.pubsub.PubSub();
  */
 
 k3d.component.PubSubTopic = 'MOVE';
+
+
 
 /**
  * The template with the lable.
@@ -42,15 +48,20 @@ k3d.component.ItemTemplate = function() {
 };
 goog.inherits(k3d.component.ItemTemplate, pstj.ui.Template);
 goog.addSingletonGetter(k3d.component.ItemTemplate);
+
+
 /** @inheritDoc */
 k3d.component.ItemTemplate.prototype.getTemplate = function(model) {
   return k3d.template.item({});
 };
+
+
 /** @inheritDoc */
 k3d.component.ItemTemplate.prototype.getContentElement = function(comp) {
   comp.getElement();
   return comp.querySelector('.' + goog.getCssName('corner-label'));
 };
+
 
 
 /**
@@ -84,6 +95,7 @@ k3d.component.Item = function(opt_template) {
 };
 goog.inherits(k3d.component.Item, pstj.ui.MoveTouch);
 
+
 /**
  * The events of the items on the drawing board.
  * @enum {string}
@@ -94,161 +106,174 @@ k3d.component.Item.EventType = {
 
 goog.scope(function() {
 
-  var _ = k3d.component.Item.prototype;
-  var css = pstj.lab.style.css;
+var _ = k3d.component.Item.prototype;
+var css = pstj.lab.style.css;
 
-  /**
-   * @override
-   * @return {pstj.ds.ListItem} The data record for the item.
-   */
-  _.getModel;
 
-  /** @inheritDoc */
-  _.setModel = function(model) {
-    goog.asserts.assertInstanceof(model, pstj.ds.ListItem, 'The data record should be a ListItem');
-    goog.base(this, 'setModel', model);
-    if (this.isInDocument()) {
-      this.dispatchEvent(k3d.component.Item.EventType.MODEL_CHANGE);
-    }
-  };
+/**
+ * @override
+ * @return {k3d.ds.Item} The data record for the item.
+ */
+_.getModel;
 
-  /** @inheritDoc */
-  _.enterDocument = function() {
-    goog.base(this, 'enterDocument');
-    this.getHandler().listen(this, pstj.ui.Touchable.EventType.MOVE,
+
+/** @inheritDoc */
+_.setModel = function(model) {
+  goog.asserts.assertInstanceof(model, pstj.ds.ListItem,
+      'The data record should be a ListItem');
+  goog.base(this, 'setModel', model);
+  if (this.isInDocument()) {
+    this.dispatchEvent(k3d.component.Item.EventType.MODEL_CHANGE);
+  }
+};
+
+
+/** @inheritDoc */
+_.enterDocument = function() {
+  goog.base(this, 'enterDocument');
+  this.getHandler().listen(this, pstj.ui.Touchable.EventType.MOVE,
       this.handleMove);
-    this.getHandler().listen(this, pstj.ui.Touchable.EventType.PRESS,
+  this.getHandler().listen(this, pstj.ui.Touchable.EventType.PRESS,
       this.handlePress);
-    this.applySizeOnDOM_();
+  this.applySizeOnDOM_();
 
-    if (goog.isDefAndNotNull(this.getModel()) &&
+  if (goog.isDefAndNotNull(this.getModel()) &&
       k3d.ds.helpers.isCornerItem(this.getModel())) {
 
-      this.getContentElement().innerHTML = 'corner';
-    } else {
-      this.getContentElement().innerHTML = '';
-    }
-  };
+    this.getContentElement().innerHTML = 'corner';
+  } else {
+    this.getContentElement().innerHTML = '';
+  }
+};
 
-  /**
-   * Handles the activate event.
-   * @param {goog.events.Event} e The ACTIVATE component event.
-   * @protected
-   */
-  _.handleActivate = function(e) {
-    // here we do need to calculate the item position...
-    goog.dom.classlist.add(this.getElement(),
+
+/**
+ * Handles the activate event.
+ * @param {goog.events.Event} e The ACTIVATE component event.
+ * @protected
+ */
+_.handleActivate = function(e) {
+  // here we do need to calculate the item position...
+  goog.dom.classlist.add(this.getElement(),
       goog.getCssName('sheet-item-active'));
-  };
+};
 
-  /**
-   * Handles the press event.
-   * @param {pstj.ui.Touchable.Event} e The PRESS event from touchables.
-   * @protected
-   */
-  _.handlePress = function(e) {
-    this.moveCache_[0] = e.x;
-    this.moveCache_[1] = e.y;
-    this.moveCache_[2] = this.moveCache_[0];
-    this.moveCache_[3] = this.moveCache_[1];
-  };
 
-  /**
-   * Do not stop the event as we use it in the sheet to switch move modes.
-   * @override
-   * @param {pstj.ui.Touchable.Event} e The long press touch event.
-   * @protected
-   */
-  _.handleLongPress = function(e) {
-    //goog.base(this, 'handleLongPress', e);
-    this.update();
-    this.setMoveEnabled(true);
-    this.activatedScaleProperty = 'scale(1.1)';
-    goog.dom.classlist.add(this.getElement(),
+/**
+ * Handles the press event.
+ * @param {pstj.ui.Touchable.Event} e The PRESS event from touchables.
+ * @protected
+ */
+_.handlePress = function(e) {
+  this.moveCache_[0] = e.x;
+  this.moveCache_[1] = e.y;
+  this.moveCache_[2] = this.moveCache_[0];
+  this.moveCache_[3] = this.moveCache_[1];
+};
+
+
+/**
+ * Do not stop the event as we use it in the sheet to switch move modes.
+ * @override
+ * @param {pstj.ui.Touchable.Event} e The long press touch event.
+ * @protected
+ */
+_.handleLongPress = function(e) {
+  //goog.base(this, 'handleLongPress', e);
+  this.update();
+  this.setMoveEnabled(true);
+  this.activatedScaleProperty = 'scale(1.1)';
+  goog.dom.classlist.add(this.getElement(),
       goog.getCssName('sheet-item-moving'));
-  };
+};
 
-  /**
-   * Handles the move event.
-   * @param {pstj.ui.Touchable.Event} e The move event abstraction.
-   * @protected
-   */
-  _.handleMove = function(e) {
-    if (this.isMoveEnabled()) {
-      e.stopPropagation();
-      this.update();
-      this.moveCache_[2] = e.x;
-      this.moveCache_[3] = e.y;
-    }
-  };
 
-  /** @inheritDoc */
-  _.handleRelease = function(e) {
-    goog.base(this, 'handleRelease', e);
-    goog.dom.classlist.remove(this.getElement(),
-      goog.getCssName('sheet-item-moving'));
+/**
+ * Handles the move event.
+ * @param {pstj.ui.Touchable.Event} e The move event abstraction.
+ * @protected
+ */
+_.handleMove = function(e) {
+  if (this.isMoveEnabled()) {
+    e.stopPropagation();
     this.update();
-    this.activatedScaleProperty = '';
-    this.moveCache_[0] = 0;
-    this.moveCache_[1] = 0;
-    this.moveCache_[2] = 0;
-    this.moveCache_[3] = 0;
-  };
+    this.moveCache_[2] = e.x;
+    this.moveCache_[3] = e.y;
+  }
+};
 
-  /** @inheritDoc */
-  _.draw = function(ms) {
-    css.setTranslation(this.getElement(),
+
+/** @inheritDoc */
+_.handleRelease = function(e) {
+  goog.base(this, 'handleRelease', e);
+  goog.dom.classlist.remove(this.getElement(),
+      goog.getCssName('sheet-item-moving'));
+  this.update();
+  this.activatedScaleProperty = '';
+  this.moveCache_[0] = 0;
+  this.moveCache_[1] = 0;
+  this.moveCache_[2] = 0;
+  this.moveCache_[3] = 0;
+};
+
+
+/** @inheritDoc */
+_.draw = function(ms) {
+  css.setTranslation(this.getElement(),
       -(this.moveCache_[0] - this.moveCache_[2]),
       -(this.moveCache_[1] - this.moveCache_[3]), undefined,
       this.activatedScaleProperty);
-    if (this.isMoveEnabled()) {
-      k3d.component.PubSub.publish(k3d.component.PubSubTopic);
-    }
-    return false;
-  };
+  if (this.isMoveEnabled()) {
+    k3d.component.PubSub.publish(k3d.component.PubSubTopic);
+  }
+  return false;
+};
 
-  /**
-   * Provides access method to the currently applied difference in position.
-   * @return {number}
-   */
-  _.getXOffset = function() {
-    return -(this.moveCache_[0] - this.moveCache_[2]);
-  };
 
-  /**
-   * Applies the internally stored size to the DOM node attached to the
-   *   component.
-   * @private
-   */
-  _.applySizeOnDOM_ = function() {
-    this.getElement().style.position = 'absolute';
-    this.getElement().style.width = this.styleoptions_[0] + '%';
-    this.getElement().style.height = this.styleoptions_[1] + '%';
-    this.getElement().style.top = this.styleoptions_[2] + '%';
-    this.getElement().style.left = this.styleoptions_[3] + '%';
-    var img = goog.asserts.assertString(this.getModel().getProp(
-        k3d.ds.definitions.Struct.DRAWING_IMAGE),
-        'Image path shoudl be string');
-    if (img == '') img = 'public/assets/1.png';
-    // this.getElement().style.backgroundImage = 'url(' + 'assets/1.png' + ')';
-    this.getElement().style.backgroundImage = 'url(' +
+/**
+ * Provides access method to the currently applied difference in position.
+ * @return {number}
+ */
+_.getXOffset = function() {
+  return -(this.moveCache_[0] - this.moveCache_[2]);
+};
+
+
+/**
+ * Applies the internally stored size to the DOM node attached to the
+ *   component.
+ * @private
+ */
+_.applySizeOnDOM_ = function() {
+  this.getElement().style.position = 'absolute';
+  this.getElement().style.width = this.styleoptions_[0] + '%';
+  this.getElement().style.height = this.styleoptions_[1] + '%';
+  this.getElement().style.top = this.styleoptions_[2] + '%';
+  this.getElement().style.left = this.styleoptions_[3] + '%';
+  var img = goog.asserts.assertString(this.getModel().getProp(
+      k3d.ds.definitions.Struct.DRAWING_IMAGE),
+      'Image path shoudl be string');
+  if (img == '') img = 'public/assets/1.png';
+  // this.getElement().style.backgroundImage = 'url(' + 'assets/1.png' + ')';
+  this.getElement().style.backgroundImage = 'url(' +
       goog.global['ASSETS_PREFIX'] + img + ')';
-  };
+};
 
-  /**
-   * Applies the size values to the child of the drawing canvas.
-   * @param {number} w The width of the child.
-   * @param {number} h The height.
-   * @param {number} l Left offset.
-   * @param {number} t Top offset.
-   */
-  _.applyStyle = function(w, h, l, t) {
-    this.styleoptions_[0] = w;
-    this.styleoptions_[1] = h;
-    this.styleoptions_[2] = t;
-    this.styleoptions_[3] = l;
-    this.styleIsDirty_ = true;
-    if (this.isInDocument()) this.update();
-  };
 
-});
+/**
+ * Applies the size values to the child of the drawing canvas.
+ * @param {number} w The width of the child.
+ * @param {number} h The height.
+ * @param {number} l Left offset.
+ * @param {number} t Top offset.
+ */
+_.applyStyle = function(w, h, l, t) {
+  this.styleoptions_[0] = w;
+  this.styleoptions_[1] = h;
+  this.styleoptions_[2] = t;
+  this.styleoptions_[3] = l;
+  //this.styleIsDirty_ = true;
+  if (this.isInDocument()) this.update();
+};
+
+});  // goog.scope
--- js/control/main.js
+++ js/control/main.js
@@ -17,6 +17,8 @@ goog.require('k3d.mb');
 goog.require('k3d.template');
 goog.require('pstj.control.Base');
 
+
+
 /**
  * Our main controller. Get an instance of it to bootstrap the application.
  * @constructor
@@ -35,7 +37,7 @@ k3d.control.Main = function() {
 
   this.getImageDelay_ = new goog.async.Delay(function() {
     k3d.control.Loader.getInstance().getPreview(this.projectid_,
-      this.previewCallback_);
+        this.previewCallback_);
   }, 500, this);
 
   /**
@@ -62,45 +64,47 @@ goog.inherits(k3d.control.Main, pstj.control.Base);
 
 goog.scope(function() {
 
-  var _ = k3d.control.Main.prototype;
+var _ = k3d.control.Main.prototype;
 
-  /** @inheritDoc */
-  _.initialize = function() {
 
-    k3d.control.ErrorHandler.getInstance().setParentControlInstance(this);
-    // first start loading data from server as this tends to be the slowest
-    // process.
-    k3d.control.Loader.getInstance().start(goog.bind(
+/** @inheritDoc */
+_.initialize = function() {
+
+  k3d.control.ErrorHandler.getInstance().setParentControlInstance(this);
+  // first start loading data from server as this tends to be the slowest
+  // process.
+  k3d.control.Loader.getInstance().start(goog.bind(
       this.onPreloadReady, this));
-  };
+};
 
-  /**
-   * At this point we have certainty that the needed data has been loaded.
-   */
-  _.onPreloadReady = function() {
-    var body = k3d.template.base({
-      links: goog.global['HEADER_LINKS'],
-      prefix: goog.global['ASSETS_PREFIX']
-    });
-    var el = goog.dom.htmlToDocumentFragment(body);
-    document.body.appendChild(el);
 
-    // find header and apply size
-    goog.dom.getElementByClass(goog.getCssName('header')).style.height =
+/**
+ * At this point we have certainty that the needed data has been loaded.
+ */
+_.onPreloadReady = function() {
+  var body = k3d.template.base({
+    links: goog.global['HEADER_LINKS'],
+    prefix: goog.global['ASSETS_PREFIX']
+  });
+  var el = goog.dom.htmlToDocumentFragment(body);
+  document.body.appendChild(el);
+
+  // find header and apply size
+  goog.dom.getElementByClass(goog.getCssName('header')).style.height =
       k3d.ds.definitions.headerHeight + 'px';
 
-    k3d.control.Buttons.getInstance().setParentControlInstance(this);
-    k3d.control.Editor.getInstance().setParentControlInstance(this);
-    k3d.control.Editor.getInstance().install(/** @type {!Element} */ (
+  k3d.control.Buttons.getInstance().setParentControlInstance(this);
+  k3d.control.Editor.getInstance().setParentControlInstance(this);
+  k3d.control.Editor.getInstance().install(/** @type {!Element} */ (
       document.body));
 
 
 
-    k3d.control.Loader.getInstance().getKitchen().addCallback(goog.bind(
+  k3d.control.Loader.getInstance().getKitchen().addCallback(goog.bind(
       function(kitchen) {
 
         goog.asserts.assertInstanceof(kitchen, k3d.ds.KitchenProject,
-          'Should have been a kitchen');
+            'Should have been a kitchen');
 
         this.projectid_ = kitchen.getId();
 
@@ -111,135 +115,140 @@ goog.scope(function() {
         //   k3d.control.Loader.getInstance().saveKitchen(kitchen);
         // }, 1000);
       }, this)
-    );
+  );
 
-    this.preview_ = new k3d.component.Preview(undefined,
+  this.preview_ = new k3d.component.Preview(undefined,
       'public/assets/bgb.jpg');
 
-    this.preview_.render(document.body);
+  this.preview_.render(document.body);
 
-    // Setup unload handler if we do not know the user.
-    if (!goog.global['USER_IS_KNOWN']) {
-      goog.events.listen(window, goog.events.EventType.BEFOREUNLOAD, function(e) {
-        setTimeout(function() {
-          k3d.mb.Bus.publish(k3d.mb.Topic.ERROR,
+  // Setup unload handler if we do not know the user.
+  if (!goog.global['USER_IS_KNOWN']) {
+    goog.events.listen(window, goog.events.EventType.BEFOREUNLOAD, function(e) {
+      setTimeout(function() {
+        k3d.mb.Bus.publish(k3d.mb.Topic.ERROR,
             k3d.ds.definitions.Static.RUNTIME, 1002);
-        }, 300);
-        var confirmationMessage = "Проектът не е запазен!";
-        (e || window.event).returnValue = confirmationMessage;
-        e.preventDefault();
-        return confirmationMessage;
-      }, undefined, this);
-    }
-
-    this.ga_();
-  };
-
-  /**
-   * Attach analytics.
-   * @private
-   */
-  _.ga_ = function() {
-    var link = document.querySelector('a[href="gagq"]');
-    var isAlreadyCounted_ = false;
-    if (goog.isNull(link)) return;
-    goog.events.listen(link, goog.events.EventType.CLICK, function(e) {
+      }, 300);
+      var confirmationMessage = 'Проектът не е запазен!';
+      (e || window.event).returnValue = confirmationMessage;
       e.preventDefault();
-      e.stopPropagation();
-      if (!isAlreadyCounted_) {
-        isAlreadyCounted_ = true;
-        if (goog.isDefAndNotNull(goog.global['_gaq'])) {
-          goog.global['_gaq'].push(['_trackEvent', 'Get Quote',
-            this.projectid_.toString()]);
-        }
+      return confirmationMessage;
+    }, undefined, this);
+  }
+
+  this.ga_();
+};
+
+
+/**
+ * Attach analytics.
+ * @private
+ */
+_.ga_ = function() {
+  var link = document.querySelector('a[href="gagq"]');
+  var isAlreadyCounted_ = false;
+  if (goog.isNull(link)) return;
+  goog.events.listen(link, goog.events.EventType.CLICK, function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    if (!isAlreadyCounted_) {
+      isAlreadyCounted_ = true;
+      if (goog.isDefAndNotNull(goog.global['_gaq'])) {
+        goog.global['_gaq'].push(['_trackEvent', 'Get Quote',
+          this.projectid_.toString()]);
       }
-      k3d.mb.Bus.publish(k3d.mb.Topic.ERROR,
+    }
+    k3d.mb.Bus.publish(k3d.mb.Topic.ERROR,
         k3d.ds.definitions.Static.STRUCTURED_ERROR, 1001,
         goog.global['QUOTE_NOT_SUPPORTED']);
-    }, undefined, this);
-  };
+  }, undefined, this);
+};
 
-  /**
-   * makes sure buttons are active only when action is possible.
-   * @private
-   */
-  _.pnc_ = function() {
-    var res = k3d.control.Editor.getInstance().isFirstOrLast();
-    if (res == 3) {
-      k3d.control.Buttons.getInstance().enable(0, true);
-      k3d.control.Buttons.getInstance().enable(5, true);
-    } else if (res == 2) {
-      k3d.control.Buttons.getInstance().enable(0, true);
-      k3d.control.Buttons.getInstance().enable(5, false);
-    } else if (res == 1) {
-      k3d.control.Buttons.getInstance().enable(0, false);
-      k3d.control.Buttons.getInstance().enable(5, true);
-    } else if (res == 0) {
-      k3d.control.Buttons.getInstance().enable(0, false);
-      k3d.control.Buttons.getInstance().enable(5, false);
+
+/**
+ * makes sure buttons are active only when action is possible.
+ * @private
+ */
+_.pnc_ = function() {
+  var res = k3d.control.Editor.getInstance().isFirstOrLast();
+  if (res == 3) {
+    k3d.control.Buttons.getInstance().enable(0, true);
+    k3d.control.Buttons.getInstance().enable(5, true);
+  } else if (res == 2) {
+    k3d.control.Buttons.getInstance().enable(0, true);
+    k3d.control.Buttons.getInstance().enable(5, false);
+  } else if (res == 1) {
+    k3d.control.Buttons.getInstance().enable(0, false);
+    k3d.control.Buttons.getInstance().enable(5, true);
+  } else if (res == 0) {
+    k3d.control.Buttons.getInstance().enable(0, false);
+    k3d.control.Buttons.getInstance().enable(5, false);
+  }
+};
+
+
+/** @inheritDoc */
+_.notify = function(child, action) {
+  if (child == k3d.control.Buttons.getInstance()) {
+    switch (action) {
+      case 'previous':
+        k3d.control.Editor.getInstance().loadSiblingWall(false);
+        this.pnc_();
+        break;
+      case 'next':
+        k3d.control.Editor.getInstance().loadSiblingWall(true);
+        this.pnc_();
+        break;
+      case 'add':
+        k3d.control.Editor.getInstance().addNewItem();
+        break;
+      case 'select-finish':
+        k3d.control.Editor.getInstance().showSelectFinishes();
+        break;
+      case 'select-handles':
+        k3d.control.Editor.getInstance().showSelectHandles();
+        break;
+      case 'save':
+        k3d.control.Loader.getInstance().assignProject(this.projectid_);
+        break;
     }
-  };
+  }
+  goog.base(this, 'notify', child, action);
+};
 
-  /** @inheritDoc */
-  _.notify = function(child, action) {
-    if (child == k3d.control.Buttons.getInstance()) {
-      switch (action) {
-        case 'previous':
-          k3d.control.Editor.getInstance().loadSiblingWall(false);
-          this.pnc_();
-          break;
-        case 'next':
-          k3d.control.Editor.getInstance().loadSiblingWall(true);
-          this.pnc_();
-          break;
-        case 'add':
-          k3d.control.Editor.getInstance().addNewItem();
-          break;
-        case 'select-finish':
-          k3d.control.Editor.getInstance().showSelectFinishes();
-          break;
-        case 'select-handles':
-          k3d.control.Editor.getInstance().showSelectHandles();
-          break;
-        case 'save':
-          k3d.control.Loader.getInstance().assignProject(this.projectid_);
-          break;
-      }
-    }
-    goog.base(this, 'notify', child, action);
-  };
 
-  /**
-   * Execute pre-loading of data AFTER the initial paint.
-   */
-  _.postLoad = function() {
+/**
+ * Execute pre-loading of data AFTER the initial paint.
+ */
+_.postLoad = function() {
 
-    // Setup the action to take when the data record changes.
-    k3d.control.Editor.getInstance().setDataChangeHandler(goog.bind(
+  // Setup the action to take when the data record changes.
+  k3d.control.Editor.getInstance().setDataChangeHandler(goog.bind(
       function(kitchen) {
         k3d.control.Loader.getInstance().saveKitchen(kitchen);
         this.getImageDelay_.start();
       }, this));
 
-    //Notify the editor when all images have been loaded, including ones on post
-    //load.
-    k3d.control.Loader.getInstance().getAllImagesLoadedDeferred().addCallback(
+  //Notify the editor when all images have been loaded, including ones on post
+  //load.
+  k3d.control.Loader.getInstance().getAllImagesLoadedDeferred().addCallback(
       function() {
         k3d.control.Editor.getInstance().onLoadComplete();
       });
 
-    goog.async.DeferredList.gatherResults([
-      k3d.control.Loader.getInstance().getKitchen(),
-      k3d.control.Loader.getInstance().getItems(),
-      k3d.control.Loader.getInstance().getFinishes(),
-      k3d.control.Loader.getInstance().getHandles()
-    ]).addCallback(function(list) {
-      var kitchen = /** @type {k3d.ds.KitchenProject} */(list[0]);
-      var finishes = /** @type {pstj.ds.List} */(list[2]);
-      var handles = /** @type {pstj.ds.List} */(list[3]);
-      k3d.control.Price.getInstance().loadData(kitchen, finishes, handles);
-    });
-    this.pnc_();
-    this.getImageDelay_.start();
-  };
-});
+  goog.async.DeferredList.gatherResults([
+    k3d.control.Loader.getInstance().getKitchen(),
+    k3d.control.Loader.getInstance().getItems(),
+    k3d.control.Loader.getInstance().getFinishes(),
+    k3d.control.Loader.getInstance().getHandles()
+  ]).addCallback(function(list) {
+    var kitchen = /** @type {k3d.ds.KitchenProject} */(list[0]);
+    var finishes = /** @type {pstj.ds.List} */(list[2]);
+    var handles = /** @type {pstj.ds.List} */(list[3]);
+    k3d.control.Price.getInstance().loadData(kitchen, finishes, handles);
+  });
+  this.pnc_();
+  this.getImageDelay_.start();
+};
+
+});  // goog.scope
