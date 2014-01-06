f9c2be4c2518b0294210d3acb0dd858b3bf447d7
Peter StJ <regardingscot@gmail.com>
2014-01-06 10:57:13 +0200

    Make the file compatible with stricter linter rules.

--- js/ds/wall.js
+++ js/ds/wall.js
@@ -4,6 +4,8 @@ goog.require('k3d.ds.CabinetRow');
 goog.require('k3d.ds.definitions');
 goog.require('pstj.ds.ListItem');
 
+
+
 /**
  * Provides the wall asbtraction data strucure. This structures represents a
  *   single wall in our design for a kitchen. It has knowledge of the rows of
@@ -31,103 +33,127 @@ goog.inherits(k3d.ds.Wall, pstj.ds.ListItem);
 
 goog.scope(function() {
 
-  var _ = k3d.ds.Wall.prototype;
-  var Struct = k3d.ds.definitions.Struct;
+var _ = k3d.ds.Wall.prototype;
+var Struct = k3d.ds.definitions.Struct;
 
-  /** @inheritDoc */
-  _.convert = function() {
-    var top = new k3d.ds.CabinetRow(
+
+/** @inheritDoc */
+_.convert = function() {
+  var top = new k3d.ds.CabinetRow(
       this.getRawData()[Struct.ITEMS][Struct.TOP_ROW]);
 
-    var bottom = new k3d.ds.CabinetRow(
+  var bottom = new k3d.ds.CabinetRow(
       this.getRawData()[Struct.ITEMS][Struct.BOTTOM_ROW]);
 
-    this.getRawData()[Struct.ITEMS][Struct.TOP_ROW] = top;
-    this.getRawData()[Struct.ITEMS][Struct.BOTTOM_ROW] = bottom;
-    this.top_ = top;
-    this.bottom_ = bottom;
-  };
+  this.getRawData()[Struct.ITEMS][Struct.TOP_ROW] = top;
+  this.getRawData()[Struct.ITEMS][Struct.BOTTOM_ROW] = bottom;
+  this.top_ = top;
+  this.bottom_ = bottom;
+};
 
-  /**
-   * Getter for the rows of cabinets as data structures.
-   * @param {boolean=} top True if we want to access the top row.
-   * @return {k3d.ds.CabinetRow}
-   */
-  _.getRow = function(top) {
-    if (top) return this.top_;
-    else return this.bottom_;
-  };
 
-  /**
-   * Iterate over all the data and return the matrix of stop points.
-   * @param {boolean} top If the stop points should be obtained from the top
-   *   or bottom row.
-   * @return {Array.<number>} The stop point matrix.
-   */
-  _.getStopPoints = function(top) {
-    if (top) return this.top_.getStopPoints();
-    return this.bottom_.getStopPoints();
-  };
+/**
+ * Getter for the rows of cabinets as data structures.
+ * @param {boolean=} opt_top True if we want to access the top row.
+ * @return {k3d.ds.CabinetRow}
+ */
+_.getRow = function(opt_top) {
+  if (opt_top) return this.top_;
+  else return this.bottom_;
+};
 
-  /**
-   * Returns all image references in this wall instance. It will walk all the
-   *   items that are currently applied on the wall (both top and floor rows)
-   *   and gather all images linked in each item applied to the wall instance.
-   * @return {Array.<string>}
-   */
-  _.getImageReferences = function() {
-    return this.top_.getImageReferences().concat(
+
+/**
+ * Abstracts and hides the manner of determining the available space for
+ * new items on a particular row on this wall.
+ * @param {boolean} top If true the returned available space will be for the
+ * top row on the wall, else it will be for the bottom (floor) row of items.
+ */
+_.getAvailableWidth = function(top) {
+  if (top) {
+
+  } else {
+    return this.getWidth() - this.bottom_.getWidth();
+  }
+};
+
+
+/**
+ * Iterate over all the data and return the matrix of stop points.
+ * @param {boolean} top If the stop points should be obtained from the top
+ *   or bottom row.
+ * @return {Array.<number>} The stop point matrix.
+ */
+_.getStopPoints = function(top) {
+  if (top) return this.top_.getStopPoints();
+  return this.bottom_.getStopPoints();
+};
+
+
+/**
+ * Returns all image references in this wall instance. It will walk all the
+ *   items that are currently applied on the wall (both top and floor rows)
+ *   and gather all images linked in each item applied to the wall instance.
+ * @return {Array.<string>}
+ */
+_.getImageReferences = function() {
+  return this.top_.getImageReferences().concat(
       this.bottom_.getImageReferences());
-  };
+};
 
-  /**
-   * Replaces an existing item on its corresponding row with a new one.
-   * @param {k3d.ds.Item} item The item to replace.
-   * @param {k3d.ds.Item} newitem The new item to use.
-   */
-  _.replaceItem = function(item, newitem) {
-    var row = this.getRowOfItem(item);
-    row.replaceItem(item, newitem);
-  };
 
-  /**
-   * Removes an item from the wall.
-   * @param {k3d.ds.Item} item The item to remove (reference).
-   */
-  _.removeItem = function(item) {
-    this.getRowOfItem(item).removeItem(item);
-  };
+/**
+ * Replaces an existing item on its corresponding row with a new one.
+ * @param {k3d.ds.Item} item The item to replace.
+ * @param {k3d.ds.Item} newitem The new item to use.
+ */
+_.replaceItem = function(item, newitem) {
+  var row = this.getRowOfItem(item);
+  row.replaceItem(item, newitem);
+};
 
-  /**
-   * Checks the cabinet for overflows. This might happen then on wall N a
-   *   corner item is added and on wall N+1 the row already has items and thus
-   *   the corner item duplicaterd on wall N+1 pushesh the cabinets outside of
-   *   the wall restrains.
-   * @return {boolean} True if the cabinets overflow the wall size.
-   */
-  _.hasOverflow = function() {
-    var width = this.getProp(Struct.WIDTH);
-    if (this.getRow(true).getWidth() > width ||
+
+/**
+ * Removes an item from the wall.
+ * @param {k3d.ds.Item} item The item to remove (reference).
+ */
+_.removeItem = function(item) {
+  this.getRowOfItem(item).removeItem(item);
+};
+
+
+/**
+ * Checks the cabinet for overflows. This might happen then on wall N a
+ *   corner item is added and on wall N+1 the row already has items and thus
+ *   the corner item duplicaterd on wall N+1 pushesh the cabinets outside of
+ *   the wall restrains.
+ * @return {boolean} True if the cabinets overflow the wall size.
+ */
+_.hasOverflow = function() {
+  var width = this.getProp(Struct.WIDTH);
+  if (this.getRow(true).getWidth() > width ||
       this.getRow(false).getWidth() > width) {
-      return true;
-    } else {
-      return false;
-    }
-  };
+    return true;
+  } else {
+    return false;
+  }
+};
 
-  /**
-   * Returns the row an item belongs to if found.
-   * @param {k3d.ds.Item} item The item to look for.
-   * @return {k3d.ds.CabinetRow} The row the item is in or null.
-   */
-  _.getRowOfItem = function(item) {
-    if (this.top_.hasItem(item)) {
-      return this.top_;
-    } else if (this.bottom_.hasItem(item)) {
-      return this.bottom_;
-    } else {
-      return null;
-    }
-  };
 
-});
+/**
+ * Returns the row an item belongs to if found.
+ * @param {k3d.ds.Item} item The item to look for.
+ * @return {k3d.ds.CabinetRow} The row the item is in or null.
+ */
+_.getRowOfItem = function(item) {
+  if (this.top_.hasItem(item)) {
+    return this.top_;
+  } else if (this.bottom_.hasItem(item)) {
+    return this.bottom_;
+  } else {
+    return null;
+  }
+};
+
+});  // goog.scope
+
